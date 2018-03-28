212785f28f50a66575c35b300f47a9063891977f
Dean Shaff <dshaff001@gmail.com>
2018-03-28 17:34:40 +0400

    Changed async examples to use newer API

--- examples/async/async_client.py
+++ examples/async/async_client.py
@@ -1,8 +1,6 @@
 import logging
 
-from test import setup_logging
-setup_logging(logging.getLogger())
-from support_pyro.support_pyro4.async.async_proxy import AsyncProxy
+from support.pyro import async
 
 called = {
     "square_callback":False,
@@ -17,20 +15,12 @@ def repeat_callback(res):
     called["repeat_callback"] = True
     print(res)
 
-AsyncProxy.register(square_callback, repeat_callback)
+uri = "PYRO:Server@localhost:50100"
 
-uri = "PYRO:Server@localhost:50001"
+client = async.AsyncProxy(uri)
 
-client = AsyncProxy(uri)
-
-# print("calling square")
-# client.square(2, callback=square_callback)
-print("calling repeat")
 client.repeat("hello", 10, callback=repeat_callback)
+client.square(2, callback=square_callback)
 
-# Because we're dealing with asynchronous responses, we need
-# some sort of external event loop.
-# while not (called["square_callback"] and called["repeat_callback"]):
-#     pass
-while not called["repeat_callback"]:
+while not called["repeat_callback"] or not called["square_callback"]:
     pass
